8270a1be182d12b74e2c8d38dcdd62eece1a9374
Richard Yance <yance0022@gmail.com>
2019-04-07 21:29:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,6 +62,12 @@ class Student
     end
   end
 
+  def self.create(name, grade)
+    new_student = self.new(name, grade)
+    new_student.save
+    new_student
+  end 
+
   def self.create_table
     sql = <<-SQL
       CREATE TABLE IF NOT EXISTS students (
